254c86a265c737d58ebc08b9f0e871f22adf9b1c
SoukainaYouss <soukainayouss03@gmail.com>
2023-12-26 18:41:55 +0100

    slider

--- enegix.js
+++ enegix.js
@@ -9,3 +9,17 @@ function scrollFunction() {
     }
 }
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
